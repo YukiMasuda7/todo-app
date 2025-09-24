577e0f7252b734ec6e03567e89c919e7bd0063a2
uniuni7 <uniuni0141@docomo.ne.jp>
2025-09-25 00:28:34 +0900

    ホーム画面の作成

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="bg-blue-400 py-3 text-3xl text-white flex justify-center">
+      <div>ToDo</div>
+    </div>
+  );
+};
+
+export default Header;
