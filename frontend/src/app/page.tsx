96d37dcc682fe0aaa2fa797c9ddfac3e5fb55060
uniuni7 <uniuni0141@docomo.ne.jp>
2025-10-03 19:55:17 +0900

    apiクラアントの設計＋todoの全取得

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,49 +1,9 @@
+import Todos from "@/components/todos";
+
 export default function Home() {
   return (
-    <div className="min-h-screen p-5">
-      <div className="bg-gray-300/50 ">
-        <ul>
-          <li className="flex items-center gap-2 p-2 border-b">
-            <input type="checkbox" className="size-5" />
-            <span className="flex-1 text-xl">タイトル</span>
-            <button className="px-2 py-1 bg-blue-500 text-white rounded">
-              詳細
-            </button>
-            <button className="px-2 py-1 bg-green-500 text-white rounded">
-              編集
-            </button>
-            <button className="px-2 py-1 bg-red-500 text-white rounded">
-              削除
-            </button>
-          </li>
-          <li className="flex items-center gap-2 p-2 border-b">
-            <input type="checkbox" className="size-5" />
-            <span className="flex-1 text-xl">タイトル</span>
-            <button className="px-2 py-1 bg-blue-500 text-white rounded">
-              詳細
-            </button>
-            <button className="px-2 py-1 bg-green-500 text-white rounded">
-              編集
-            </button>
-            <button className="px-2 py-1 bg-red-500 text-white rounded">
-              削除
-            </button>
-          </li>
-          <li className="flex items-center gap-2 p-2 border-b">
-            <input type="checkbox" className="size-5" />
-            <span className="flex-1 text-xl">タイトル</span>
-            <button className="px-2 py-1 bg-blue-500 text-white rounded">
-              詳細
-            </button>
-            <button className="px-2 py-1 bg-green-500 text-white rounded">
-              編集
-            </button>
-            <button className="px-2 py-1 bg-red-500 text-white rounded">
-              削除
-            </button>
-          </li>
-        </ul>
-      </div>
+    <div>
+      <Todos />
     </div>
   );
 }
