d25128d1ac25511289ee845ce5b54299d44ac235
uniuni7 <uniuni0141@docomo.ne.jp>
2025-10-06 02:48:26 +0900

    新規作成・削除の実装

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 
-const Header = () => {
+interface HeaderProps {
+  onCreateClick: () => void;
+}
+
+const Header: React.FC<HeaderProps> = ({ onCreateClick }) => {
   return (
-    <div className="bg-blue-400 py-3 text-3xl text-white flex justify-center">
-      <div>ToDo</div>
+    <div className="bg-blue-400 py-3 text-3xl text-white flex items-center">
+      <div className="w-1/3"></div>
+      <div className="w-1/3 text-center">ToDo</div>
+      <div className="w-1/3 flex justify-end pr-4">
+        <button
+          className="px-4 py-2 text-lg bg-amber-500 hover:bg-amber-600 rounded cursor-pointer"
+          onClick={onCreateClick}
+        >
+          新規作成
+        </button>
+      </div>
     </div>
   );
 };
